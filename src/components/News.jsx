eec1591a5dd031c0999136c98b6c486b4c62a2c2
vishal kumar <vk9491015@gmail.com>
2025-11-06 15:53:42 +0530

    add navbar.jsx

---
+++ src/components/News.jsx
@@ -0,0 +1,191 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem';
+export default class News extends Component {
+ article=[
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "reuters.com",
+"title": "Russia's Tuapse halted fuel exports after drone attacks, refinery stopped, sources say",
+"description": "",
+"url": "https://biztoc.com/x/a3c9302267861277",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:33:01Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+756 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "reuters.com",
+"title": "Super Micro shares slip as delivery delays stall AI momentum",
+"description": "",
+"url": "https://biztoc.com/x/0305cda4af014a0d",
+"urlToImage": "https://biztoc.com/cdn/0305cda4af014a0d_s.webp",
+"publishedAt": "20251105T09:32:53Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+727 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "reuters.com",
+"title": "German auto sector sentiment markedly improves in October, finds survey",
+"description": "",
+"url": "https://biztoc.com/x/3a4e7fca33bae9c0",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:32:50Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+743 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "reuters.com",
+"title": "Italy retail sales fall 0.5% m/m in September",
+"description": "",
+"url": "https://biztoc.com/x/28bb5b3c685a9684",
+"urlToImage": "https://biztoc.com/cdn/28bb5b3c685a9684_s.webp",
+"publishedAt": "20251105T09:21:59Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+709 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "investing.com",
+"title": "Elevated valuations in focus; AMD reports; Novo’s outlook  what’s moving markets",
+"description": "",
+"url": "https://biztoc.com/x/22e097400337c195",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:21:58Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+749 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "bloomberg.com",
+"title": "Hedge Funds That Dodged Climate Rules in Europe Take Aim at UK",
+"description": "",
+"url": "https://biztoc.com/x/fef0e6bc60378fd1",
+"urlToImage": "https://biztoc.com/cdn/fef0e6bc60378fd1_s.webp",
+"publishedAt": "20251105T09:21:49Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+734 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Slashdot.org"
+},
+"author": "feedfeeder",
+"title": "Tesla Is Obsessed With Musk’s Pay Package. Musk Is Obsessed With AI.  The Wall Street Journal",
+"description": "Tesla Is Obsessed With Musk’s Pay Package. Musk Is Obsessed With AI.The Wall Street Journal Major Tesla Investor Rejects Elon Musk’s $1 Trillion Pay DealThe Wall Street Journal Norwegian opposition complicates Musk's path to $1 trillion pay dealReuters The Pe…",
+"url": "https://slashdot.org/firehose.pl?op=view&amp;id=179970408",
+"urlToImage": null,
+"publishedAt": "20251105T09:12:19Z",
+"content": "The Fine Print: The following comments are owned by whoever posted them. We are not responsible for them in any way."
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "nytimes.com",
+"title": "Donald Trump’s China Trade Policy Is a Hot Mess",
+"description": "",
+"url": "https://biztoc.com/x/eff3eeaea69ed05e",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:11:22Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+699 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "barrons.com",
+"title": "Finding Investing Heirs to Warren Buffett Will Be Difficult",
+"description": "",
+"url": "https://biztoc.com/x/5d7ffa95555faed2",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:11:21Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+715 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "cato.org",
+"title": "Striking Down the Tariffs Won't Hurt the Economy or Budget",
+"description": "",
+"url": "https://biztoc.com/x/0773478342163a56",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:11:21Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+694 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "thehill.com",
+"title": "Supreme Court Should Liberate Us From 'Liberation Day'",
+"description": "",
+"url": "https://biztoc.com/x/f635cd227561257b",
+"urlToImage": "https://biztoc.com/cdn/950/og.png",
+"publishedAt": "20251105T09:11:17Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+706 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Biztoc.com"
+},
+"author": "bloomberg.com",
+"title": "Why Is Nvidia the King of AI Chips, and Can It Last?",
+"description": "",
+"url": "https://biztoc.com/x/83fca95839f8cc73",
+"urlToImage": "https://biztoc.com/cdn/83fca95839f8cc73_s.webp",
+"publishedAt": "20251105T09:11:14Z",
+"content": "{ window.open(this.href, '_blank'); }, 200); return false;\"&gt;Will Tesla shareholders approve Musks $1 trillion pay? { window.open(this.href, '_blank'); }, 200); return false;\"&gt;How is US governme… [+754 chars]"
+},
+]
+  render() {
+    return (
+      <>
+        <h1>News Monkeys -- Top Heading</h1>
+        <div className="container">
+            <div className="row my-3 mx-3">
+          <div className="col">
+            <Newsitem title="Title 1" description="Description 1" />
+           
+          </div>
+
+          <div className="col">
+            <Newsitem title="Title 1" description="Description 1" />
+     
+          </div>
+          
+          <div className="col">
+            <Newsitem title="Title 1" description="Description 1" />
+        
+          </div>
+
+      
+        </div>
+    
+      </div>
+  </>
+    
+  )
+}
+}
